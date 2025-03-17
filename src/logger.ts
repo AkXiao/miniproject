202a059a55c57ca89718c4b3d0348d007fadf89f
AkXiao <aomkwan1234@yahoo.com>
2025-03-17 11:30:05 +0700

    Delete src directory

--- src/logger.ts
+++
@@ -1,8 +0,0 @@
-import debug from 'debug'
-
-export const logFarmGrid = debug('farm-grid')
-export const logFarmTile = debug('farm-tile')
-export const logFarmLayout = debug('farm-layout')
-export const logFarmStatus = debug('farm-status')
-export const logFarmShop = debug('farm-shop')
-export const logFarmProgress = debug('farm-progress')
